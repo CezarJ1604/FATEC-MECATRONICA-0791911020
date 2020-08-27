9fa83986f51fde300d5eb1863be385af9fbf9639
CezarJ1604 <69973640+CezarJ1604@users.noreply.github.com>
2020-08-27 10:54:13 -0300

    Create programa05.py

---
+++ Prática02/programa05.py
@@ -0,0 +1,13 @@
+#Determinar se um número é par ou ímpar
+numero = int(input("Numero:"))
+#Calcula o resto da divisão de número por 2
+resto = numero % 2
+
+#Condição
+if resto == 0:
+  print("PAR")
+  print("Vencedor")
+  print("Nao desista de seus objetivos")
+  print("O mindset é a chave do sucesso")
+
+print("Esse sempre aparece!")
